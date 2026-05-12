c453fb9294ffb18f51f697de31e8ef020939c883
elafraya <elafraya@fsedome.com>
2026-05-12 18:03:54 +0200

    app añadida

--- src/services/api.js
+++ src/services/api.js
@@ -1,58 +1,86 @@
 const READ_URL =
-  "https://script.google.com/macros/s/AKfycbyvDkWjcwLs5kc8kRenIa5S_GMD_Gi1o5Qpj5puZ1eELtymRvPNar2Ph_3V-B-Mne-oPA/exec"
+    "https://script.google.com/macros/s/AKfycbyvDkWjcwLs5kc8kRenIa5S_GMD_Gi1o5Qpj5puZ1eELtymRvPNar2Ph_3V-B-Mne-oPA/exec"
 
-const WRITE_URL =
-  "https://sheetdb.io/api/v1/d315esql3sk7q"
+const PRIMARY_WRITE_URL =
+    "https://sheetdb.io/api/v1/jicyvu20q0lfl"
+
+const BACKUP_WRITE_URL =
+    "https://sheetdb.io/api/v1/mliu5cqq5ca32"
 
 export async function getExpenses() {
 
-  const response = await fetch(
-    `${READ_URL}?sheet=expenses`
-  )
+    const response = await fetch(
+        `${READ_URL}?sheet=expenses`
+    )
 
-  return await response.json()
+    return await response.json()
 }
 
 export async function getParticipants() {
 
-  const response = await fetch(
-    `${READ_URL}?sheet=participants`
-  )
+    const response = await fetch(
+        `${READ_URL}?sheet=participants`
+    )
 
-  return await response.json()
+    return await response.json()
 }
 
 export async function getConfig() {
 
-  const response = await fetch(
-    `${READ_URL}?sheet=config`
-  )
+    const response = await fetch(
+        `${READ_URL}?sheet=config`
+    )
 
-  return await response.json()
+    return await response.json()
 }
 
 export async function createExpense(expense) {
 
-  await fetch(
-    `${WRITE_URL}?sheet=expenses`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        data: expense
-      })
-    }
-  )
+    await fetchWithFallback(
+        `${PRIMARY_WRITE_URL}?sheet=expenses`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+            body: JSON.stringify({
+                data: expense
+            })
+        }
+    )
 }
 
 export async function removeExpense(id) {
 
-  await fetch(
-    `${WRITE_URL}/id/${id}?sheet=expenses`,
-    {
-      method: "DELETE"
+    await fetchWithFallback(
+        `${PRIMARY_WRITE_URL}/id/${id}?sheet=expenses`,
+        {
+            method: "DELETE"
+        }
+    )
+}
+
+async function fetchWithFallback(
+    url,
+    options
+) {
+
+    let response =
+        await fetch(url, options)
+
+    if (
+        response.status === 429
+    ) {
+
+        response = await fetch(
+            url.replace(
+                PRIMARY_WRITE_URL,
+                BACKUP_WRITE_URL
+            ),
+            options
+        )
     }
-  )
+
+    return response
 }
